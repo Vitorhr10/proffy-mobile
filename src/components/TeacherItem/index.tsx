94cb17daec0d8f787400cb02fb9ccae0e3779327
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2020-08-09 22:23:05 -0300

    teacher favorite

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,7 +3,7 @@ import { View, Image, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 
 import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
-import unfavoriteIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
@@ -36,8 +36,9 @@ function TeacherItem() {
         </Text>
 
         <View style={styles.buttonsContainer}>
-          <RectButton style={styles.favoriteButton}>
-            <Image source={heartOutlineIcon}/>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            {/* <Image source={heartOutlineIcon}/> */}
+            <Image source={unfavoriteIcon} />
           </RectButton>
 
           <RectButton style={styles.contactButton}>
